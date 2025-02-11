bc9dc8a137d66ffce9cc15aa4231f1e161aa7b1b
Kunwar-awadhiya <kunwarawadhiya@gmail.com>
2025-02-11 18:35:39 +0530

    new changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,27 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 
 const Home = () => {
+
+  const addToCartHandler = () => {}
+
   return (
-    <div>
-      Home
+    <div className="home">
+      <section></section>
+
+
+      <h1>
+        Latest Products
+        <Link to={'/search'} className="findMore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        
+      </main>
     </div>
-  )
-}
+  );
+};
 
 export default Home
